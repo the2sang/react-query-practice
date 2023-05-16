9791987b8d748310baf541c9d7f68310bef372ea
KHC <the2sang@gmail.com>
2023-05-16 17:03:41 +0900

    Main Page suc

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,19 @@
-
 'use client'
 
 import "./globals.css"
-import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import {useState} from "react";
 import MainCategoryPage from "@/app/MainCategory/page";
 
 
-declare module "react-query/types/react/QueryClientProvider" {
-  interface QueryClientProviderProps {
-    children?: React.ReactNode;
-  }
+const Home = () => {
+    return (
+        <>
+            {/*<Component {...pageProps} />*/}
+            {/*<Link href="/ParallelQureisMainCategory">Paralel Link</Link>*/}
+            <MainCategoryPage/>
+        </>
+    )
 }
 
-export default function Home() {
-
-  const [queryClient] = useState(() => new QueryClient())
-
-  return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        {/*<Component {...pageProps} />*/}
-        <MainCategoryPage />
-        <ReactQueryDevtools initialIsOpen={false} position="bottom-left" />
-      </QueryClientProvider>
-    </>
-  )
-}
-
-// export default Page
+export default Home
